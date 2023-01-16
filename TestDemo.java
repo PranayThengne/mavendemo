ce05a299c52580ec0c26a85acd0daf30553a2b16
PranayThengne <pranaythengne96@gmail.com>
2023-01-16 18:50:11 +0530

    bug second time is fixed

--- TestDemo.java
+++ TestDemo.java
@@ -1,7 +1,9 @@
 
 public class TestDemo {
-
+private static String name="Thengne";
 	public static void main(String[] args) {
+		System.out.println("hi pranay");
 		System.out.println("We have solved our bug");
+		System.out.println("hi"+name);
 	}
 }
